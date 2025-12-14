019bb39c81031c6098efdc18d6928648c23d1962
shagungarg01 <79191139+shagungarg01@users.noreply.github.com>
2025-12-14 11:52:19 +0530

    Add files via upload

---
+++ app/(main)/person/[id]/page.jsx
@@ -0,0 +1,118 @@
+"use client"
+import React,{useState} from 'react';
+import { api } from '../../../../convex/_generated/api';
+import { useConvexQuery } from '../../../../hooks/use-convex-query';
+import { BarLoader } from 'react-spinners';
+import { Button } from '../../../../components/ui/button';
+import { ArrowLeft, ArrowLeftRight, ArrowRight, PlusCircle } from 'lucide-react';
+import Link from 'next/link';
+import { CardHeader, Card, CardContent, CardTitle } from '../../../../components/ui/card';
+import { ExpenseList } from '../../../../components/expense-list';
+import { SettlementList } from '../../../../components/settlement-list';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useParams, useRouter } from "next/navigation";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+const PersonPage = () => {
+    const params = useParams();
+    const router = useRouter();
+    const [activeTab, setActiveTab] = useState("expenses");
+
+    const {data, isLoading} = useConvexQuery(
+        api.expenses.getExpensesBetweenUsers,
+        {userId: params.id}
+    );
+
+    if(isLoading){
+        return(
+            <div className='container mx-auto py-12'>
+                <BarLoader width={"100%"} color='#36d7b7'/>
+            </div>
+        )
+    }
+
+    const otherUser = data?.otherUser;
+    const expenses = data?.expenses || [];
+    const settlements = data?.settlements || [];
+    const balance = data?.balance || 0;
+  return (
+    <div>
+        <div>
+            <Button variant="outline" size="sm" onClick={() => router.back()} className="mb-4">
+                <ArrowLeft className='h-4 w-4 mr-2'/>
+                Back
+            </Button>
+            <div className='flex items-center justify-between'>
+                <div className='flex items-center gap-3'>
+                    <Avatar className="h-16 w-16">
+                        <AvatarImage src={otherUser?.imageUrl}/>
+                        <AvatarFallback>
+                            {otherUser?.name?.charAt(0) || "?"}
+                        </AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <h1 className='text-4xl gradient-title'>{otherUser?.name}</h1>
+                        <p className='text-muted-foreground'>{otherUser?.email}</p>
+                    </div>
+                </div>
+
+                <div className='flex gap-2'>
+                    <Button asChild variant="outline">
+                        <Link href={`/settlements/user/${params.id}`}>
+                            <ArrowLeftRight className='mr-2 h-4 w-4'/>
+                             Settle up
+                        </Link>
+                    </Button>
+                    <Button asChild>
+                        <Link href={`/expenses/new`}>
+                            <PlusCircle className='mr-2 h-4 w-4'/>
+                            Add expense
+                        </Link>
+                    </Button>
+                </div>
+            </div>
+        </div>
+        <Card className="mb-6">
+            <CardHeader className="pb-2"> 
+                <CardTitle className="text-xl">Balance</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <div>
+                    <div>
+                        {balance === 0 ? (
+                            <p>You are all settled up</p>
+                        ) : balance > 0 ? (
+                            <p>
+                                <span className='font-medium'>{otherUser?.name} </span>
+                                owes you
+                            </p>
+                        ):(
+                            <p>You owe <span className='font-medium'>{otherUser?.name}</span></p>
+                        )}
+                    </div>
+                    <div className={`text-2xl font-bold ${balance > 0 ? "text-green-600" : balance<0 ? "text-red-600" : ""}`}>
+                        ${Math.abs(balance).toFixed(2)}
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
+
+        <Tabs defaultValue="expenses" className="space-y-4" value={activeTab} onValueChange={setActiveTab}>
+            <TabsList className="grid w-full grid-cols-2">
+                <TabsTrigger value="expenses">Expenses ({expenses.length})</TabsTrigger>
+                <TabsTrigger value="settlement">Settlements ({settlements.length})</TabsTrigger>
+            </TabsList>
+            <TabsContent value="account">Make changes to your account here.
+                <ExpenseList expenses={expenses} showOtherPerson={false} otherPersonId={params.id} userLookupMap={{[otherUser.id]: otherUser}}/>
+            </TabsContent>
+            <TabsContent value="password">
+                <SettlementList 
+                    settlements={settlements}
+                    userLookupMap={{ [otherUser.id] : otherUser}}
+                />
+            </TabsContent>
+        </Tabs>
+    </div>
+  )
+}
+
+export default PersonPage;
---
+++ app/layout.js
@@ -0,0 +1,30 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "../components/header";
+import { ConvexClientProvider } from "../components/convex-client-provider";
+import {ClerkProvider} from "@clerk/nextjs";
+
+const inter = Inter({subsets: ["latin"]});
+
+export const metadata = {
+  title: "Splitr",
+  description: "The smartest way to split expenses with friends",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/logos/logo-s.png" sizes="any" />
+      </head>
+      <body className={`${inter.className}`}>
+        <ClerkProvider>
+        <ConvexClientProvider>
+        <Header/>
+        <main className="min-h-screen">{children}</main>
+        </ConvexClientProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
